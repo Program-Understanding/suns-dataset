958b6659d42ac466d656b0ef36934ffad6ddbfae
cdohara <23649165+cdohara@users.noreply.github.com>
2025-02-01 12:06:32 -0600

    toggle mode for addr_to_offset, add ignoreme, add cmp_indr test

--- control_flow_recovery/analysis/tools/jakstab-cfg/addr_to_offset.py
+++ control_flow_recovery/analysis/tools/jakstab-cfg/addr_to_offset.py
@@ -1,8 +1,8 @@
 import struct
 #import logging
 import argparse
-# To convert virtual address to file offset, we need to parse the header
-def virtual_to_file_offset(filepath, virtual_addr):
+
+def parse_header(filepath):
     with open(filepath, 'rb') as f:
         # validate dos signature
         if f.read(2) != b'MZ':
@@ -30,21 +30,35 @@ def virtual_to_file_offset(filepath, virtual_addr):
         # get raw offset
         raw_offset = struct.unpack('<I', f.read(4))[0]
 
-        file_offset = virtual_addr - base_addr - section_offset + raw_offset
-        print(f"{hex(file_offset)} = {hex(virtual_addr)} - {hex(base_addr)} - {hex(section_offset)} + {hex(raw_offset)}")
-        return file_offset
+        return base_addr, section_offset, raw_offset
+
+def virtual_to_file_offset(virtual_addr, base_addr, section_offset, raw_offset):
+    file_offset = virtual_addr - base_addr - section_offset + raw_offset
+    return file_offset
+
+def file_to_virtual_address(file_offset, base_addr, section_offset, raw_offset):
+    virtual_addr = base_addr + section_offset - raw_offset + file_offset
+    return virtual_addr
 
 def main():
-    parser = argparse.ArgumentParser(description='Converts virtual address to file offset')
+    parser = argparse.ArgumentParser(description='Converts virtual address to file offset and vice versa')
     parser.add_argument("filepath", help="a filepath to a PE file")
-    parser.add_argument("vaddr", help="virtual address to convert to file offset")
+    parser.add_argument("hexorint", help="string value of a hex or int representing either virtual address or file offset")
     args = parser.parse_args()
-    vaddr = 0
-    if "0x" in args.vaddr:
-        vaddr = int(args.vaddr, 16)
-    else:
-        vaddr = int(args.vaddr)
-    virtual_to_file_offset(args.filepath, vaddr)
 
+    # check if string input is given in hex
+    num = 0
+    if "0x" in args.hexorint:
+        num = int(args.hexorint, 16)
+    else:
+        num = int(args.hexorint)
+
+    base_addr, section_offset, raw_offset = parse_header(args.filepath)
+    if num < base_addr + section_offset:
+        vaddr = file_to_virtual_address(num, base_addr, section_offset, raw_offset)
+        print(hex(vaddr))
+    else:
+        foff = virtual_to_file_offset(num, base_addr, section_offset, raw_offset)
+        print(hex(foff))
 if __name__ == "__main__":
     main()
