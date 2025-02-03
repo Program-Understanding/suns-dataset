736f9229e9a6334d44bd2cda934d4ac1c1a42a2b
cdohara <23649165+cdohara@users.noreply.github.com>
2025-02-02 22:11:34 -0600

    add cmp_indr-1-cfr.json, working jakstab tool

--- control_flow_recovery/analysis/tools/jakstab-cfg/jakstab.py
+++ control_flow_recovery/analysis/tools/jakstab-cfg/jakstab.py
@@ -1,9 +1,62 @@
 import argparse
 import json
 import re
+import subprocess
+import fnmatch
+import struct
+import pydot
 
 
-def parse_cfr(cfrpath):
+def main():
+    parser = argparse.ArgumentParser(description='Jakstab runner')
+    parser.add_argument("cfrpath", help="filepath to a *-cfr.sjon file")
+    args = parser.parse_args()
+    
+    program, groundtruth, instr, offset = _parse_cfr(args.cfrpath)
+    
+    # run addr_to_offset.py with arguments program and offset to get virt addr
+    # check if string input is given in hex
+    num = 0
+    if "0x" in offset:
+        num = int(offset, 16)
+    else:
+        num = int(offset)
+
+    base_addr, section_offset, raw_offset = _parse_header(program)
+    if num < base_addr + section_offset:
+        vaddr = _file_to_virtual_address(num, base_addr, section_offset, raw_offset)
+        print(hex(vaddr))
+    else:
+        foff = _virtual_to_file_offset(num, base_addr, section_offset, raw_offset)
+        print(hex(foff))
+    base_addr, section_off, raw_off = _parse_header(program)
+
+    # run /jakstab/jakstab -m program --cpa ocbfikrsvx
+    subprocess.run(["/jakstab/jakstab", "-m", program, "--cpa", "ocbfikrsvx"])
+
+    # run parse_jakstab.py with arguments program and virtaddr
+    dotfiles = _process_program_name(program)
+    destinations = _parse_graph(dotfiles, str(hex(vaddr))[2:])
+
+    # do instruction sanity check
+    print(f"instruction was {instr}.. assert is not implemented yet")
+
+    # compare values in groundtruth to the results from parse_jakstab
+    print(f"RESULTS: The groundtruth is: {groundtruth}")
+    print(f"RESULTS: The tool's answer is: {destinations}")
+    print("RESULTS: ", end='')
+    if set(groundtruth) == destinations:
+        print("YES :D")
+    else:
+        print("NO :(")
+
+###################################################################
+#
+# Internal functions
+#
+###################################################################
+
+def _parse_cfr(cfrpath):
     if not '-cfr.json' in cfrpath:
         raise FileNotFoundError("the provided filepath does not contain -cfr.json")
 
@@ -34,24 +87,44 @@ def parse_cfr(cfrpath):
 
     return program, groundtruth, instruction_string, offset
 
-def main():
-    parser = argparse.ArgumentParser(description='Jakstab runner')
-    parser.add_argument("cfrpath", help="filepath to a *-cfr.sjon file")
-    args = parser.parse_args()
+def _file_to_virtual_address(file_offset, base_addr, section_offset, raw_offset):
+    virtual_addr = base_addr + section_offset - raw_offset + file_offset
+    return virtual_addr
+
+def _virtual_to_file_offset(virtual_addr, base_addr, section_offset, raw_offset):
+    file_offset = virtual_addr - base_addr - section_offset + raw_offset
+    return file_offset
+
+def _parse_header(filepath):
+    with open(filepath, 'rb') as f:
+        # validate dos signature
+        if f.read(2) != b'MZ':
+            raise ValueError("Missing DOS signature")
+
+        # move to pe header offset
+        f.seek(0x3C)
+        # read as little-endian unsigned int
+        pe_offset = struct.unpack('<I', f.read(4))[0]
+        
+        # validate PE header
+        f.seek(pe_offset)
+        if f.read(4) != b'PE\x00\x00':
+            raise ValueError("Missing PE signature")
+
+        # get base address (usually 0x400000)
+        f.seek(pe_offset + 0x34)
+        base_addr = struct.unpack('<I', f.read(4))[0]
+        
+        # there is an assumption here that this is in the .text section, and that the .text
+        # section is the first section
+        # get section/module offset
+        section_offset = struct.unpack('<I', f.read(4))[0]
+
+        # get raw offset
+        raw_offset = struct.unpack('<I', f.read(4))[0]
+
+        return base_addr, section_offset, raw_offset
     
-    program, groundtruth, instr, offset = parse_cfr(args.cfrpath)
-    
-    # run addr_to_offset.py with arguments program and offset to get virt addr
-
-    # run /jakstab/jakstab -m program --cpa ocbfikrsvx
-
-    # run parse_jakstab.py with arguments program and virtaddr
-
-    # do instruction sanity check
-
-    # compare values in groundtruth to the results from parse_jakstab
-
-
 def _process_question(question):
     question_prefixes = {
         "What are the file offsets for the instructions that are the targets of the": "targets",
@@ -79,3 +152,40 @@ def _process_question(question):
         raise NotImplementedError("The question is not fully understood, perhaps spacing is off?")
 
     return question_type, quoted_strings
+
+def _process_program_name(program):
+    # remove extension if exists
+    if ".exe" in program:
+        program = program.rsplit(".", 1)[0]
+    
+    # there should only be two dot files emitted from jakstab
+    f1 = program + "_asmcfg.dot"
+    f2 = program + "_cfa.dot"
+    dotfiles = [f1, f2]
+
+    return dotfiles
+
+def _parse_graph(dotfiles, vaddr):
+    destinations = set()
+
+    for dotfile in dotfiles:
+        g = pydot.graph_from_dot_file(dotfile)[0]
+
+        for e in g.get_edges():
+            src = e.get_source()
+            dst = e.get_destination()
+            
+            if fnmatch.fnmatch(src, f'*{vaddr}*'):
+                destinations.add(hex(int(dst[3:-3],16)))
+
+    if vaddr in destinations:
+        destinations.remove(vaddr)
+
+    print(f"\nDestination Nodes: [count: {len(destinations)}]")
+    for targets in destinations:
+        print(targets)
+    
+    return destinations
+
+if __name__ == "__main__":
+    main()
