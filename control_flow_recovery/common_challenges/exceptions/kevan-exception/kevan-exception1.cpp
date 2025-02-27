4bffcedd38812a36a18327d2eeefa016ffefed3e
bakerk98 <baker.kevan98@gmail.com>
2025-02-27 14:39:36 -0600

    Updated bins

--- control_flow_recovery/common_challenges/exceptions/kevan-exception/kevan-exception1.cpp
+++ control_flow_recovery/common_challenges/exceptions/kevan-exception/kevan-exception1.cpp
@@ -4,22 +4,22 @@
 /* Simple example to throw an exception from another function and catch in main*/
 
 float divide(int numerator, int denominator) {
-    if (denominator == 0) {
-        std::cout << "Division by zero detected in perform_operation function." << std::endl;
-        throw std::runtime_error("Division by zero exception");
-    }
-    return float(numerator) / float(denominator);
+  if (denominator == 0) {
+    throw std::runtime_error("Division by zero exception");
+  }
+  return float(numerator) / float(denominator);
 }
 
 int main() {
   int numerator, denominator;
   std::cin >> numerator;
   std::cin >> denominator;
-    try {
-        divide(numerator,denominator);
-        divide(numerator,denominator);
-    } catch (const std::runtime_error& e) {
-        std::cout << "Exception caught in main function: " << e.what() << std::endl;
-    }
-    return 0;
+  float (*f_ptr)(int);
+  try {
+    float result = divide(numerator,denominator);
+  } catch (const std::runtime_error &e) {
+    float result = 
+  }
+  std::cout << "Result" << &f_ptr(result);
+  return 0;
 }
