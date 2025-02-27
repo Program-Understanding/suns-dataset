a050fc1f66ae62dda34d6fb5feb0e0f8edf98332
bakerk98 <baker.kevan98@gmail.com>
2025-02-27 14:40:29 -0600

    Updated src

--- control_flow_recovery/common_challenges/exceptions/kevan-exception/kevan-exception2.cpp
+++ control_flow_recovery/common_challenges/exceptions/kevan-exception/kevan-exception2.cpp
@@ -4,23 +4,27 @@
 /* Throw an exception from a function called indirectly by main */
 
 float divide(int numerator, int denominator) {
+  //separate division function
   if (numerator == 0) {
     throw std::runtime_error("Division by zero exception");
   }
   return float(numerator) / float(denominator);
 }
 
-float intermediary(int numerator, int denominator) {
+float intermediary() {
+  // intermediary function that i want to stay in the binary
+  int numerator, denominator;
+  std::cout << "Input the numerator as an integer" << std::endl;
+  std::cin >> numerator;
+  std::cout << "Input the denominator as an integer" << std::endl;
+  std::cin >> denominator;
   return divide(numerator, denominator);
 }
 
 int main() {
-  int numerator, denominator;
-  std::cin >> numerator;
-  std::cin >> denominator;
+  //main calls intermediary, which calls divide, which then comes back to main
   try {
-    intermediary(numerator, denominator);
-    intermediary(numerator, denominator);
+    std::cout << "Division result is: " << intermediary() << std::endl;
   } catch (const std::runtime_error& e) {
     std::cout << "Exception caught in main function: " << e.what() << std::endl;
   }
