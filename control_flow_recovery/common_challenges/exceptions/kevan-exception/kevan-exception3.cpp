cefad153ad923df698a503c85002cf1a6c7215ef
bakerk98 <baker.kevan98@gmail.com>
2025-03-20 13:23:16 -0500

    3 new bins that follow Unwind_RaiseException

--- control_flow_recovery/common_challenges/exceptions/kevan-exception/kevan-exception3.cpp
+++ control_flow_recovery/common_challenges/exceptions/kevan-exception/kevan-exception3.cpp
@@ -1,37 +1,144 @@
 #include <iostream>
+#include <exception>
 
-/* Throw an exception from a function called by main which is caught and then thrown again to main */
+/* Throw an exception and handle it within the statically linked catch */
+class CustomException : public std::exception {
+public:
+  const char *what() const noexcept override {
+    return "Custom exception called!";
+  }
+  virtual int value() const noexcept {
+    return 0;
+  }
+};
 
-void foo(int val) {
-  std::cout << "Foo called with val " << val << std::endl;
-  throw 1;
+class Foo : public CustomException {
+public:
+  Foo(int val) : _value(val) {}
+  const char *what() const noexcept override {
+    return "Foo called";
+  }
+  int value() const noexcept override {
+    return _value;
+  }
+private:
+  int _value;
+};
+// void foo(int val) {
+//   std::cout << "Foo called with val " << val << std::endl;
+//   throw 1;
+// }
+// class Bar : public CustomException {
+// public:
+//   Bar(int val) : _value(val) {}
+//   const char *what() const noexcept override {
+//     return "Bar called";
+//   }
+//   int value() const noexcept override {
+//     return _value;
+//   }
+// private:
+//   int _value;
+// };
+// // void bar(int val) {
+// //   std::cout << "Bar called with val " << val << std::endl;
+// //   throw 2;
+// // }
+
+// class Fizz : public CustomException {
+// public:
+//   Fizz(int val) : _value(val) {}
+//   const char *what() const noexcept override {
+//     return "Fizz called";
+//   }
+//   int value() const noexcept override {
+//     return _value;
+//   }
+// private:
+//   int _value;
+// };
+
+// class Buzz : public CustomException {
+// public:
+//   Buzz(int val) : _value(val) {}
+//   const char *what() const noexcept override {
+//     return "Buzz called";
+//   }
+//   int value() const noexcept override {
+//     return _value;
+//   }
+// private:
+//   int _value;
+// };
+
+// void fizz(int val) {
+//   std::cout << "Fizz called with val " << val << std::endl;
+//   throw 3;
+// }
+
+// void buzz(int val) {
+//   std::cout << "Buzz called with val " << val << std::endl;
+// }
+
+// void inter(void (*funs[4])(int), int val) {
+//   try {
+//     funs[val](val);
+//   }
+//   catch (int &e) {
+//     inter(funs,e);
+//   }
+// }
+
+// void inter(int val) {
+//   try {
+//     if (val % 4 == 0) {
+//       throw Foo(val);
+//     } else if (val % 3 == 0) {
+//       throw Bar(val);
+//     }
+//     else if (val % 2 == 0) {
+//       throw Fizz(val);
+//     } else {
+//       throw Buzz(val);
+//     }
+//   } catch (CustomException &e) {
+//     std::cout << e.what() << " with value " << e.value() << std::endl;
+//   }
+// }
+
+// int main() {
+//   void (*funs[4])(int) = {foo, bar, fizz, buzz};
+//   inter(funs,0);
+//   return 0;
+// }
+
+// int main(int argc, char **argv) {
+//   int val;
+//   if (argc > 1)
+//     val = atoi(argv[1]);
+//   inter(val);
+// }
+
+void potential_error(int val) {
+  if (val % 2 == 0)
+    throw Foo(val);
 }
 
-void bar(int val) {
-  std::cout << "Bar called with val " << val << std::endl;
-  throw 2;
-}
-
-void fizz(int val) {
-  std::cout << "Fizz called with val " << val << std::endl;
-  throw 3;
-}
-
-void buzz(int val) {
-  std::cout << "Buzz called with val " << val << std::endl;
-}
-
-void inter(void (*funs[4])(int), int val) {
+int main(int argc, char **argv) {
+  int val = 1;
+  if (argc > 1)
+    val = atoi(argv[1]);
   try {
-    funs[val](val);
+    potential_error(val);
   }
-  catch (int &e) {
-    inter(funs,e);
+  catch (CustomException &e){
+    std::cout << e.what() << " given the value " << e.value() << std::endl;
   }
-}
 
-int main() {
-  void (*funs[4])(int) = {foo, bar, fizz, buzz};
-  inter(funs,0);
-  return 0;
+  try {
+    potential_error(val);
+  }
+  catch (CustomException &e){
+    std::cout << e.what() << " with the value " << e.value() << std::endl;
+  }
 }
--- control_flow_recovery/common_challenges/exceptions/kevan-exception/kevan-exception4.cpp
+++ control_flow_recovery/common_challenges/exceptions/kevan-exception/kevan-exception4.cpp
@@ -1,46 +1,101 @@
 #include <iostream>
-#include <cstdlib>
+#include <exception>
 
-/* Throw an exception with a class object */
+/* Throw an exception which can go to multiple catch blocks.
+   More difficult than kevan-exception3 through using different functions
+   in the try blocks */
 
-class KevClass {
+// class KevClass {
+// public:
+//   void performOperation(int operationCode) {
+//     if (operationCode == 0) {
+//       throw 1;
+//     } else if (operationCode == 1) {
+//       throw 2;
+//     } else if (operationCode == 2){
+//       throw 3;
+//     } else {
+//       throw 4;
+//     }
+//   }
+// };
+
+
+// int main() {
+//   KevClass kev;
+//   std::cout << "Enter an integer" << std::endl;
+//   int user_num;
+//   std::cin >> user_num;
+//   try {
+//     kev.performOperation(user_num);
+//   } catch (int &e) {
+//     switch (e) {
+//     case 1:
+//       std::cout << "Handling error code 1" << std::endl;
+//       break;
+//     case 2:
+//       std::cout << "Handling error code 2" << std::endl;
+//       break;
+//     case 3:
+//       std::cout << "Handling error code 3" << std::endl;
+//       break;
+//     default:
+//       std::cout << "Unknown error code" << std::endl;
+//       break;
+//     }
+//   }
+//   return 0;
+// }
+
+class KevException : public std::exception {
 public:
-  void performOperation(int operationCode) {
-    if (operationCode == 0) {
-      throw 1;
-    } else if (operationCode == 1) {
-      throw 2;
-    } else if (operationCode == 2){
-      throw 3;
-    } else {
-      throw 4;
-    }
+  const char *what() const noexcept override {
+    return "Custom exception called!";
+  }
+  virtual int value() const noexcept {
+    return 0;
   }
 };
 
-
-int main() {
-  KevClass kev;
-  std::cout << "Enter an integer" << std::endl;
-  int user_num;
-  std::cin >> user_num;
-  try {
-    kev.performOperation(user_num);
-  } catch (int &e) {
-    switch (e) {
-    case 1:
-      std::cout << "Handling error code 1" << std::endl;
-      break;
-    case 2:
-      std::cout << "Handling error code 2" << std::endl;
-      break;
-    case 3:
-      std::cout << "Handling error code 3" << std::endl;
-      break;
-    default:
-      std::cout << "Unknown error code" << std::endl;
-      break;
-    }
+class KevExcption : public KevException {
+public:
+  KevExcption(int val) : _value(val) {}
+  const char *what() const noexcept override {
+    return "Separate, different kind of exception called!";
   }
-  return 0;
+  int value() const noexcept override {
+    return _value;
+  }
+
+private:
+  int _value;
+};
+
+void potential_error(int val) {
+  if (val % 2 == 0)
+    throw KevExcption(val);
+}
+
+void another_potential_error(int val) {
+  if (val % 3 == 0)
+    throw KevExcption(val);
+}
+
+int main(int argc, char **argv) {
+  int val;
+  if (argc > 1)
+    val = atoi(argv[1]);
+  try {
+    potential_error(val);
+  }
+  catch (KevException &e) {
+    std::cout << e.what() << " with the value " << e.value() << std::endl;
+  }
+
+  try {
+    another_potential_error(val);
+  }
+  catch (KevException &e){
+    std::cout << e.what() << " given the value " << e.value() << std::endl;
+  }
 }
