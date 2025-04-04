d5f1db2100417f8020f8aa1a7ca86d9459b54aef
bakerk98 <baker.kevan98@gmail.com>
2025-04-04 10:28:12 -0500

    Updated exceptions, added README

--- control_flow_recovery/common_challenges/exceptions/kevan-exception/kevan-exception4.cpp
+++ control_flow_recovery/common_challenges/exceptions/kevan-exception/kevan-exception4.cpp
@@ -5,48 +5,6 @@
    More difficult than kevan-exception3 through using different functions
    in the try blocks */
 
-// class KevClass {
-// public:
-//   void performOperation(int operationCode) {
-//     if (operationCode == 0) {
-//       throw 1;
-//     } else if (operationCode == 1) {
-//       throw 2;
-//     } else if (operationCode == 2){
-//       throw 3;
-//     } else {
-//       throw 4;
-//     }
-//   }
-// };
-
-
-// int main() {
-//   KevClass kev;
-//   std::cout << "Enter an integer" << std::endl;
-//   int user_num;
-//   std::cin >> user_num;
-//   try {
-//     kev.performOperation(user_num);
-//   } catch (int &e) {
-//     switch (e) {
-//     case 1:
-//       std::cout << "Handling error code 1" << std::endl;
-//       break;
-//     case 2:
-//       std::cout << "Handling error code 2" << std::endl;
-//       break;
-//     case 3:
-//       std::cout << "Handling error code 3" << std::endl;
-//       break;
-//     default:
-//       std::cout << "Unknown error code" << std::endl;
-//       break;
-//     }
-//   }
-//   return 0;
-// }
-
 class KevException : public std::exception {
 public:
   const char *what() const noexcept override {
