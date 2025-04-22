f48a89bab750106130379487ba1e93b70470baf3
Robb Matzke <matzke1@llnl.gov>
2025-04-22 10:06:19 -0400

    Added two more simple-switch curated tests
    * consecutive.c: split the 10-case switch into two consecutive
      5-case switches. This is interesting because some compilers
      will generate a single combined jump table for the two `switch`
      statements.
    
    * nested.c: split the original 10-case switch into two `switch`
      statements with the second nested in one of the cases of the
      first. I.e., an outer and innner switch. This is interesting because
      the indirect control flow needs to be discovered in two phases when
      doing recursive descent discovery: we need to know the control flow of
      the outer switch in order to find the inner switch.

---
+++ control_flow_recovery/currated/simple-switch/consecutive.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+  switch (argc) {
+  case 0:
+    printf("case 0\n");
+    break;
+  case 1:
+    printf("case 1\n");
+    break;
+  case 2:
+    printf("case 2\n");
+    break;
+  case 3:
+    printf("case 3\n");
+    break;
+  case 4:
+    printf("case 4\n");
+    break;
+  }
+
+  switch (argc) {
+  case 5:
+    printf("case 5\n");
+    break;
+  case 6:
+    printf("case 6\n");
+    break;
+  case 7:
+    printf("case 7\n");
+    break;
+  case 8:
+    printf("case 8\n");
+    break;
+  case 9:
+    printf("case 9\n");
+    break;
+  default:
+    printf("default\n");
+  }
+
+  exit(0);
+
+}
