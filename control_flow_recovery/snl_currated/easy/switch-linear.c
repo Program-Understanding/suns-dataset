562afbe4421353867ae8fcfc8d3ec9582aaba062
TodAmon <ttamon@sandia.gov>
2025-05-08 14:11:00 -0600

    updates to switch-linear, new switch-dead needs truth

--- control_flow_recovery/snl_currated/easy/switch-linear.c
+++ control_flow_recovery/snl_currated/easy/switch-linear.c
@@ -3,41 +3,51 @@
 #include <string.h>
 
 int main(int argc, char *argv[]) {
-  switch (argc) {
+  char* msg;
+  int x = (argc + 23) * (argc + 6) + argc;
+  if (argc <= 0) {
+    exit(0);
+  }
+  switch (argc-1) {
   case 0:
-    printf("case 0\n");
+    msg = "case 1";
+    x = x + 234;
     break;
   case 1:
-    printf("case 1\n");
+    msg = "case 2";
+    x = x - 345;
     break;
   case 2:
-    printf("case 2\n");
+    msg = "case 3";
+    x = x * 456;
     break;
   case 3:
-    printf("case 3\n");
+    msg = "case 4";
+    x = x / 567; 
     break;
   case 4:
-    printf("case 4\n");
+    msg = "case 5";
+    x = x + 678;
     break;
   case 5:
-    printf("case 5\n");
+    msg = "case 6";
+    x = x - 789;
     break;
   case 6:
-    printf("case 6\n");
+    msg = "case 7";
+    x = x * 890;
     break;
   case 7:
-    printf("case 7\n");
+    msg = "case 8";
+    x = x / 901;
     break;
   case 8:
-    printf("case 8\n");
-    break;
-  case 9:
-    printf("case 9\n");
+    msg = "case 9";
     break;
   default:
-    printf("default\n");
+    msg = "default";
   }
-
+  printf("It was %s and x is %d\n",msg,x);
   exit(0);
 
 }
