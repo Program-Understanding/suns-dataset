08ef0abba7accde7021ccaad941127680f355551
TullisNelsonAU <jtn0022@auburn.edu>
2025-05-29 00:39:15 -0500

    Callbacks

--- control_flow_recovery/sunbench25/source/indirect_calls/callbacks/callbacks6.c
+++ control_flow_recovery/sunbench25/source/indirect_calls/callbacks/callbacks6.c
@@ -1,17 +1,19 @@
 /* foreach.c */
 #include <stdint.h>
 
-#define NOINLINE_NOCLONE __attribute__((noinline, noclone))
+// #define NOINLINE_NOCLONE __attribute__((noinline, noclone))
 
 typedef void (*each_cb)(int *);
 
-static NOINLINE_NOCLONE void for_each(int *a, int n, each_cb fn)
+// static NOINLINE_NOCLONE 
+void for_each(int *a, int n, each_cb fn)
 {
     for (int i = 0; i < n; ++i)
         fn(&a[i]);
 }
 
-static NOINLINE_NOCLONE void negate(int *p) { *p = -*p; }
+// static NOINLINE_NOCLONE 
+void negate(int *p) { *p = -*p; }
 
 int main(void)
 {
