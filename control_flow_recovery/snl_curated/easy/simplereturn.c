a7c4f23e4d784745b8bfdfe3d1f57a434ef7518c
TodAmon <ttamon@sandia.gov>
2025-06-30 13:20:24 -0600

    add simple return

---
+++ control_flow_recovery/snl_curated/easy/simplereturn.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int thefunc(int num) {
+  return num*num;
+}
+
+int a(int num) {
+  return thefunc(num);
+}
+
+int b(int num) {
+  return thefunc(num*2);
+}
+
+int c(int num) {
+  return thefunc(num*3);
+}
+
+int main(int argc, char *argv[]) {
+  int sum = 0;
+  sum = sum + a(argc);
+  sum = sum + b(argc);
+  sum = sum + c(argc);
+  printf("result: %d\n", sum);
+  return 0;
+}
