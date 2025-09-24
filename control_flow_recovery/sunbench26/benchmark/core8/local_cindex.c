4437f28d718eac7dda9befc9925552fec6656618
TodAmon <ttamon@sandia.gov>
2025-09-24 10:17:07 -0600

    add the core8 as experiment

---
+++ control_flow_recovery/sunbench26/benchmark/core8/local_cindex.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int x = 0;
+int f1() { return 1;}
+int f2() { return 2;}
+int f3() { return 3;}
+int f4() { return 4;}
+int main() {
+  int (*fp[3])() = {f1,f2,f3};
+  x = x + f1(); x = x + fp[1]();
+  x = x + f1() + f2() + f3() + f4();
+  printf("%d",x);
+}
