34750c8b4e700ea58cd47e78ca350e28fa3703bf
TodAmon <ttamon@sandia.gov>
2025-09-26 12:22:17 -0600

    add loops

---
+++ control_flow_recovery/sunbench26/benchmark/core8/local_loop.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int x = 0;
+int f1() { return 1;}
+int f2() { return 2;}
+int f3() { return 3;}
+int f4() { return 4;}
+int main() {
+  int (*fp[3])() = {f1,f2,f3};
+  for (int i = 0; i < 2; i++) {
+    x = x + fp[i]();
+  }
+  x = x + 2*fp[f4()-2]();
+  printf("%d",x);
+}
